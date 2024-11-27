87f43664e61efe5c433a868c414ad2bd0e0940ee
Ahualla Molina Alonso <aahuallam@ucvvirtual.edu.pe>
2024-11-27 08:46:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+function irAMedio() {
+    window.location.href = 'medio.html'; // Navegar a la página Medio
+  }
+  
+  function irAInicio() {
+    window.location.href = 'index.html'; // Navegar a la página Inicio
+  }
+  
+  function irAFin() {
+    window.location.href = 'fin.html'; // Navegar a la página Fin
+  }
+  
